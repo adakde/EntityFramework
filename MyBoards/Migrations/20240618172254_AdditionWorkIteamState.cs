b949939983b863a92d0170f9772165aa900bdb0f
adakde <kuba-kasprzak12@wp.pl>
2024-06-18 19:32:46 +0200

    Update

---
+++ MyBoards/Migrations/20240618172254_AdditionWorkIteamState.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionWorkIteamState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(table: "States", column: "Value", value: "On Hold");
+            migrationBuilder.InsertData(table: "States", column: "Value", value: "Rejected");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(table: "States", keyColumn: "Value", keyValue: "On Hold");
+            migrationBuilder.DeleteData(table: "States", keyColumn: "Value", keyValue: "Rejected");
+        }
+    }
+}
